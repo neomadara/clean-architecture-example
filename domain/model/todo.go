dfae4ea99b90fda8cb4083750f4e8d9ceb67436f
Cristian Gutiérrez <instanciageek@gmail.com>
2021-04-20 23:16:16 -0400

    get Todo by ID (feat): add ID primitive objectID to model

--- domain/model/todo.go
+++ domain/model/todo.go
@@ -1,7 +1,10 @@
 package model
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type Todo struct {
-	Title     string `json:"title"`
-	Desc      string `json:"description"`
-	Completed bool   `json:"completed"`
+	ID        primitive.ObjectID `bson:"_id, omitempty"`
+	Title     string             `json:"title"`
+	Desc      string             `json:"description"`
+	Completed bool               `json:"completed"`
 }
