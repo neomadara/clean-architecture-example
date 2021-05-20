989fbe502241a4111e0b9c9bc9e9b626c270c403
Cristian Gutiérrez <instanciageek@gmail.com>
2021-05-19 22:45:37 -0400

    no-history: add delete todo route

--- infrastructure/router/router.go
+++ infrastructure/router/router.go
@@ -10,5 +10,6 @@ func NewRouter(app *fiber.App, c controller.AppController) *fiber.App {
 	app.Get("/todos/:id", func(ctx *fiber.Ctx) error { return c.Todo.FindTodoById(ctx) })
 	app.Post("/todos", func(ctx *fiber.Ctx) error { return c.Todo.CreateTodo(ctx) })
 	app.Put("/todos/:id", func(ctx *fiber.Ctx) error { return c.Todo.UpdateTodo(ctx) })
+	app.Delete("/todos/:id", func(ctx *fiber.Ctx) error { return c.Todo.DeleteTodo(ctx) })
 	return app
 }
