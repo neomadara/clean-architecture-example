c0f71ef1816b59ed687f89e9f6f0f1673af2af5b
Cristian Gutiérrez <instanciageek@gmail.com>
2021-05-19 22:51:48 -0400

    no-history: expose _id

--- domain/model/todo.go
+++ domain/model/todo.go
@@ -1,7 +1,10 @@
 package model
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type Todo struct {
-	Title     string `json:"title"`
-	Desc      string `json:"desc"`
-	Completed bool   `json:"completed"`
+	Id        primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	Title     string             `json:"title"`
+	Desc      string             `json:"desc"`
+	Completed bool               `json:"completed"`
 }
