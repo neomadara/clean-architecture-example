cd80d82102f1cc00f3d9573e25c0be3b08fe3f37
Cristian Gutiérrez <instanciageek@gmail.com>
2021-05-21 23:22:07 -0400

    no-history: default value for Completed attribute

--- domain/model/todo.go
+++ domain/model/todo.go
@@ -6,5 +6,5 @@ type Todo struct {
 	Id        primitive.ObjectID `json:"id" bson:"_id,omitempty"`
 	Title     string             `json:"title"`
 	Desc      string             `json:"desc"`
-	Completed bool               `json:"completed"`
+	Completed bool               `json:"completed" default:"false"`
 }
